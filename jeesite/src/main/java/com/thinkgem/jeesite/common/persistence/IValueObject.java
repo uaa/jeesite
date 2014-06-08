e004f4d3086ba0e4ba41a7a9e0dfe4ac2da3dd18
uaa <mfkfkulkwxf@163.com>
2014-06-08 16:08:46 +0800

    d

---
+++ jeesite/src/main/java/com/thinkgem/jeesite/common/persistence/IValueObject.java
@@ -0,0 +1,28 @@
+/**  
+ * 全球陶瓷艺术（数字）馆 
+ * 功能描述： 
+ * 
+ * <p>版权所有：dorsia 
+ * <p>未经本公司许可，不得以任何方式复制或使用本程序任何部分
+ *
+ * @author:Peter  2012-11-9 
+ * @version 1.0.0  
+ */
+package com.thinkgem.jeesite.common.persistence;
+
+import java.io.Serializable;
+
+/**
+ * 全球陶瓷艺术（数字）馆 功能描述：
+ * 
+ * <p>
+ * 版权所有：dorsia
+ * <p>
+ * 未经本公司许可，不得以任何方式复制或使用本程序任何部分
+ * 
+ * @author:Peter 2012-11-9
+ * @version 1.0.0
+ */
+
+public interface IValueObject extends Serializable {
+}
